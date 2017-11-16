2d746cc9089a5eb1964b0b41f1977b35d0533ea2
Julian <jarangol@eafit.edu.co>
2017-11-16 15:25:25 -0500

    version inicial

---
+++ spark_kmeans.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+# tf-idf
+from pyspark import SparkContext
+from pyspark.mllib.feature import HashingTF, IDF
+
+# KMeans
+from numpy import array
+from math import sqrt
+from pyspark.mllib.clustering import KMeans, KMeansModel
+
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="Alexander_Julian")  # SparkContext
+
+    documents = sc.wholeTextFiles("hdfs://...")
+    documents_values = documents.values.map(lambda line: line.split(" "))
+
+    hashingTF = HashingTF()
+    tf = hashingTF.transform(documents_values)
+
+    idf = IDF().fit(tf)
+    tfidf = idf.transform(tf)
+
+    print("tfidf:")
+    for each in tfidf.collect():
+        print(each)
+sc.stop()
